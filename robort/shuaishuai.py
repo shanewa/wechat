386794a0f4d9a398695f54e0ad148b596ed1c5c5
Shane Wang <30461642+shanewa@users.noreply.github.com>
2018-04-05 11:05:12 +0800

    Update shuaishuai.py

--- robort/shuaishuai.py
+++ robort/shuaishuai.py
@@ -6,7 +6,7 @@ from itchat.content import *
 @itchat.msg_register([TEXT, MAP, PICTURE, CARD, SHARING], isFriendChat=True)
 def text_reply(msg):
     # cont = alice.respond(msg['Text'])
-    cont = requests.get('http://www.tuling123.com/openapi/api?key=5666370b22c542cba3c9c9e6ed93fda1&info=%s' % msg['Content']).content
+    cont = requests.get('http://www.tuling123.com/openapi/api?key=(replace_your_key)&info=%s' % msg['Content']).content
     m = json.loads(cont)
     itchat.send(m['text'], msg['FromUserName'])
     if m['code'] == 200000:
